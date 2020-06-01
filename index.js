7f78965f569a114b5fdfa6300a72d5b06da6e127
KaviiSuri <surikavii@gmail.com>
2020-06-01 13:29:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let username = "";
+let email = "";
+let password = "";
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  username = req.body.username;
+  email = req.body.email;
+  password = req.body.password;
+  res.redirect("/profile");
+});
+
+app.get("/profile", (req, res) => {
+  res.json({
+    username,
+    email,
+    password,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on PORT 3000");
+});
